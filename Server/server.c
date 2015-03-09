a1faf8bf0e46979e99af6e1d09f4983014e03a95
Stefanus Anggara <anggara.stefanus@gmail.com>
2015-03-09 14:50:02 +0700

    test1
    test1

---
+++ Server/server.c
@@ -0,0 +1,15 @@
+// how to compile
+// gcc -o <nama_hasil_ekseskusi> <file_name>.c
+// contoh: gcc -o server_result server.c
+// ./<nama hasil eksekusi> (untuk run)
+// contoh: ./server_result
+
+#include <stdio.h>
+
+int main(int argcc, char *argv[])
+{
+	printf("Hello Guys\n");
+
+	return 0;
+}
+
