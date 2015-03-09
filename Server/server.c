c8bda4b9b0b5ee2e4178bd9c2083fb104f331b25
diagnosa <d.fenomena@live.com>
2015-03-09 15:07:30 +0700

    socket

--- Server/server.c
+++ Server/server.c
@@ -4,12 +4,72 @@
 // ./<nama hasil eksekusi> (untuk run)
 // contoh: ./server_result
 
-#include <stdio.h>
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<errno.h>
+#include<string.h>
+#include<stdlib.h>
 
-int main(int argcc, char *argv[])
-{
-	printf("Hello Guys\n");
+void main(int argc, char** argv){
+	int sockfd, sockcli;
+	int clisize;
+	struct sockaddr_in servaddr, cliaddr;
 
-	return 0;
+	if((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		printf("Socket failed\n");
+	}
+	else
+	{
+		printf("Socket created\n");
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(6064);
+	
+	printf("COBA\n");
+	
+	if(bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+	{
+		printf("Bind failed\n");
+	}
+	else
+	{
+		printf("Bind to port %s\n", servaddr.sin_port);
+	}
+	
+	printf("COBA\n");
+	
+	if(listen(sockfd, 5) < 0)
+	{
+		printf("Listen failed\n");
+	}
+	else
+	{
+		printf("Listening to client\n");
+	}
+	
+	printf("COBA\n");
+	
+	for(;;)
+	{
+		memset(&cliaddr, 0, sizeof(cliaddr));
+	
+		clisize = sizeof(cliaddr);
+		
+		if((sockcli = accept(sockfd, (struct sockaddr *)&cliaddr, &clisize)) < 0)
+		{
+			printf("Accept failed\n");
+		}
+		else
+		{
+			printf("Ada panggilan dari %s\n", inet_ntoa(cliaddr.sin_addr));
+			
+		}
+	}
+	close(sockfd);
 }
 
